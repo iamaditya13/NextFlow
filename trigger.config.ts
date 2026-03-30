028b3787b090336654e0839fafcf89be5d99d5b4
Aditya <your-email@example.com>
2026-03-31 01:46:31 +0530

    Debug

--- trigger.config.ts
+++ trigger.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from '@trigger.dev/sdk/v3'
 import { ffmpeg } from '@trigger.dev/build/extensions/core'
-import { getTriggerProjectId } from './src/lib/env/getTriggerEnv'
-
-const projectId = getTriggerProjectId({ allowMissing: true }) || '__MISSING_TRIGGER_PROJECT_ID__'
 
 export default defineConfig({
-  project: projectId,
+  project: 'proj_zvdwgcvkkdlmmprxcctd',
   runtime: 'node',
   logLevel: 'log',
   maxDuration: 300,
